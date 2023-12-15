cb69dc93ebdb39b5a22edda47afd3d3f21f2ad01
Aditya Mishra <adityam.rtm@gmail.com>
2023-12-15 09:09:38 +0530

    polish notes UI

--- index.js
+++ index.js
@@ -1,23 +1,28 @@
+let cs = 0;
 
-
-let currentSpan = 0;
+window[`s${cs}`].classList.add("editing");
+window.ie.scrollIntoView({ block: "center" });
 
 function handleInput(event) {
+	window[`s${cs}`].innerText = window.ie.value;
 	if (event.keyCode === 13) {
+		window[`s${cs}`].classList.remove("editing");
 		if (event.shiftKey) {
-			currentSpan = Math.max(currentSpan - 1, 0);
+			cs = Math.max(cs - 1, 0);
 		} else {
-			currentSpan++;
+			cs++;
 		}
-		if (!window[`s${currentSpan}`]) {
+		if (!window[`s${cs}`]) {
 			let s = document.createElement("span");
-			s.setAttribute("id", `s${currentSpan}`);
+			s.setAttribute("id", `s${cs}`);
+			s.innerText = " ";
 			document.body.appendChild(s);
 		}
-		window[`s${currentSpan}`].after(window["ie"]);
-	} else {
-
+		window[`s${cs}`].after(window.ie);
+		window.ie.value = window[`s${cs}`].innerText;
+		window[`s${cs}`].classList.add("editing");
 	}
+	window.ie.scrollIntoView({ behavior: "smooth", block: "center", inline: "nearest" });
 }
 
 
