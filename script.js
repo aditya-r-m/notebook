18b884661f673239f323253b6452f13ae5523803
Aditya Mishra <adityam.rtm@gmail.com>
2023-12-16 01:27:02 +0530

    implement load+save wiring

--- script.js
+++ script.js
@@ -6,27 +6,34 @@ function typeset(el) {
 
 let cs = 0;
 function createSpan() {
-	if (!window[`s${cs}o`]) {
+	if (!window[`s${cs}i`]) {
 		let s = document.createElement("span");
 		s.setAttribute("id", `s${cs}i`);
 		document.body.appendChild(s);
 		window[`s${cs}i`].classList.add("span-input");
+	}
+	if (!window[`s${cs}o`]) {
 		s = document.createElement("span");
 		s.setAttribute("id", `s${cs}o`);
 		document.body.insertBefore(s, window.ie);
 		window[`s${cs}o`].classList.add("span-output");
-		window[`s${cs}o`].classList.add("editing");
 	}
+	window[`s${cs}o`].innerText = window[`s${cs}i`].innerText;
+	typeset(window[`s${cs}o`]);
 }
-createSpan();
+let inputSpanLength = document.getElementsByClassName('span-input').length;
+for (cs = 0; cs < Math.max(1, inputSpanLength); cs++) {
+	createSpan();
+}
+cs--;
+window[`s${cs}o`].classList.add("editing");
 
 function centerInput() {
 	setTimeout(() => window.ie.scrollIntoView({ behavior: "smooth", block: "center" }));
 }
 
 function handleInput(event) {
-	window[`s${cs}i`].innerText = window.ie.value;
-	window[`s${cs}o`].innerText = window.ie.value;
+	window[`s${cs}i`].innerText = window[`s${cs}o`].innerText = window.ie.value;
 	typeset(window[`s${cs}o`]);
 	if (event.keyCode === 13) {
 		window[`s${cs}o`].classList.remove("editing");
@@ -52,6 +59,6 @@ setInterval(function() {
       },
       body: Array.from(
 			document.getElementsByClassName('span-input'))
-		.map(x => x.innerHTML).join('\n<span>\n')
-    }).catch(_ => {});
+		.map(x => x.outerHTML).join('\n')
+    });
 }, 2048);
--- server.py
+++ server.py
@@ -1,34 +1,51 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from os import listdir
 
-hostName = "localhost"
-serverPort = 8080
+host_name = "localhost"
+server_port = 8080
 
-class MyServer(BaseHTTPRequestHandler):
+class Server(BaseHTTPRequestHandler):
     def do_GET(self):
-        if self.path == '/': self.path = '/index.html'
+        mime = self.path.split('.')[-1]
         self.send_response(200)
-        self.send_header("Content-type", f"text/{self.path.split('.')[-1]}")
+        self.send_header("Content-type", f"text/{mime}")
         self.end_headers()
-        if self.path.endswith('list.html'):
-            self.wfile.write(bytes("\n".join(listdir("./data")), "utf-8"))
-        with open(f"./{self.path}", 'r') as requested_file:
-            self.wfile.write(bytes(requested_file.read(), "utf-8"))
+        if mime == "html":
+            self.wfile.write(bytes("<html>", "utf-8"))
+            with open("./head.html", 'r') as requested_file:
+                self.wfile.write(bytes(requested_file.read(), "utf-8"))
+            self.wfile.write(bytes("<body>", "utf-8"))
+            with open("./input.html", 'r') as requested_file:
+                self.wfile.write(bytes(requested_file.read(), "utf-8"))
+            try:
+                with open(f"./data/{self.path}", 'r') as requested_file:
+                    self.wfile.write(bytes(requested_file.read(), "utf-8"))
+            except: pass
+            self.wfile.write(bytes("</body>", "utf-8"))
+            self.wfile.write(bytes("</html>", "utf-8"))
+        else:
+            with open(f"./{self.path}", 'r') as requested_file:
+                self.wfile.write(bytes(requested_file.read(), "utf-8"))
+
 
     def do_PUT(self):
         length = int(self.headers['Content-Length'])
         with open(f"./{self.path}", 'wb') as f:
             f.write(self.rfile.read(length))
         self.send_response(201, "Created")
+        self.end_headers()
 
 if __name__ == "__main__":
-    webServer = HTTPServer((hostName, serverPort), MyServer)
-    print("Server started http://%s:%s" % (hostName, serverPort))
 
-    try:
-        webServer.serve_forever()
-    except KeyboardInterrupt:
-        pass
+    web_server = HTTPServer((host_name, server_port), Server)
+    print("Server started.")
+    print(f"Create/Edit notebooks on: http://{host_name}:{server_port}/<notebook>")
 
-    webServer.server_close()
+    for notebook in listdir("./data"):
+        print(f"Notebook available http://{host_name}:{server_port}/{notebook}")
+
+    try: web_server.serve_forever()
+    except KeyboardInterrupt: pass
+
+    web_server.server_close()
     print("Server stopped.")
